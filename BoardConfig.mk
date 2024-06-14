492764cf51f27cc6dfc0f47f42e34182a9c0dabd
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    Z01R: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,16 +1,7 @@
-# Copyright (C) 2018-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (C) 2018-2024 The LineageOS Project
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 BOARD_VENDOR := asus
@@ -122,7 +113,8 @@ TARGET_USES_ION := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
